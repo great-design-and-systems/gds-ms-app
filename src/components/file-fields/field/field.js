c09ceb51edd9602aa24d1d591c14a1bb25e3bfb9
rickzx98 <rickzx98@gmail.com>
2016-10-16 01:53:18 +0800

    updates

--- src/components/file-fields/field/field.js
+++ src/components/file-fields/field/field.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import RemoveFileField from '../actions/remove-file-field';
 import ReloadFileFields from '../actions/reload-file-fields';
-import { Link } from 'react-router';
 export default class Field extends React.Component {
     constructor() {
         super();
+        this.state = {};
+    }
+    componentWillMount() {
+        this.setState({
+            path: '#field/' + this.props.field.value + '/' + this.props.fileId + '/' + this.props.fieldId
+        })
     }
     removeFileField() {
         new RemoveFileField(this.props.fileId, this.props.fieldId, err => {
@@ -15,7 +20,7 @@ export default class Field extends React.Component {
     }
     render() {
         return (
-            <li class="tabs-title"><Link to={'path/${this.props.field.value}/${this.props.fileId}/${this.props.fieldId}'} class="column large-9 medium-9 small-9 field">{this.props.field.value}</Link><a onClick={this.removeFileField.bind(this)} class="column large-3 medium-3 small-3">remove</a></li>
+            <li class="tabs-title"><a href={this.state.path} class="column large-9 medium-9 small-9 field">{this.props.field.value}</a><a onClick={this.removeFileField.bind(this)} class="column large-3 medium-3 small-3">remove</a></li>
         );
-    }
+    } s
 }
